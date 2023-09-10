ff0e1934f5fc202606890c88057c9e3ef6a3b28d
Cédric Leonel <cedricleonelkouam@gmail.com>
2023-09-10 22:58:16 +0200

    Create first DTO for user entity

---
+++ src/main/java/com/formation/banking/dto/UserDto.java
@@ -0,0 +1,45 @@
+package com.formation.banking.dto;
+
+import com.formation.banking.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+    public static UserDto fromEntity(User user){
+        return UserDto.builder()
+                .id(user.getId())
+                .firstname(user.getFirstname())
+                .lastname(user.getLastname())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+
+    public static User toEntity(UserDto user){
+        return User.builder()
+                .id(user.getId())
+                .firstname(user.getFirstname())
+                .lastname(user.getLastname())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+
+}
