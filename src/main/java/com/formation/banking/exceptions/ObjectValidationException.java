ba258781312704ba12da703218b7dc9bd21a1086
Cédric Leonel <cedricleonelkouam@gmail.com>
2023-09-21 15:52:04 -0400

    Set getter for properties

--- src/main/java/com/formation/banking/exceptions/ObjectValidationException.java
+++ src/main/java/com/formation/banking/exceptions/ObjectValidationException.java
@@ -1,5 +1,6 @@
 package com.formation.banking.exceptions;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
@@ -8,6 +9,7 @@ import java.util.Set;
  * This is an exception raise in case of violation
  * It's help to have more information than the generic ones given by the dependencies **/
 @RequiredArgsConstructor
+@Getter
 public class ObjectValidationException extends RuntimeException{
 
     private final Set<String> errorMessages;
