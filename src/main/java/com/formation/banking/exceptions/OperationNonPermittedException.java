a8f2724642e23c96da98b88db048fb84272a29fe
Cédric Leonel <cedricleonelkouam@gmail.com>
2023-11-05 12:05:15 -0500

    add exception for non permitted operations

---
+++ src/main/java/com/formation/banking/exceptions/OperationNonPermittedException.java
@@ -0,0 +1,16 @@
+package com.formation.banking.exceptions;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class OperationNonPermittedException extends RuntimeException{
+
+    private final String errorMsg;
+
+    private final String source;
+
+    private final String dependency;
+
+}
