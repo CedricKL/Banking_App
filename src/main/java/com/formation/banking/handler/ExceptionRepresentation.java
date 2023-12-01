ae5f883d18df0acd8a04ca530085be2592ad8900
Cédric Leonel <cedricleonelkouam@gmail.com>
2023-12-01 17:10:06 -0500

    add getter to show values

--- src/main/java/com/formation/banking/handler/ExceptionRepresentation.java
+++ src/main/java/com/formation/banking/handler/ExceptionRepresentation.java
@@ -5,9 +5,11 @@ package com.formation.banking.handler;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.Set;
 
+@Getter
 @Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
